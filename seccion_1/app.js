3decdfb0b2eff8e98d7325b4f6060358e1bbc419
INAUTX <sanchitos1065@gmail.com>
2024-04-18 21:28:01 -0500

    segundo commit

--- seccion_1/app.js
+++ seccion_1/app.js
@@ -1,2 +1,15 @@
-const name = prompt("Por favor ingrese su nombre: ");
-const lastName = prompt("Por favor ingrese su apellido: ");
+let fullName = prompt("Por favor ingrese su nombre completo: ");
+let result = fullName.toLowerCase();
+const dominio = "@myDomain.com"
+console.log(result)
+const list = result.split(" ");
+let userName = list[0].slice(0 , 3) + list[1].slice(0 , 3); 
+let email = list[0].slice(0 , 3) + list[1].slice(0 , 3) + dominio; 
+console.log("Nombre de usuario: " + userName);
+console.log("Correo Electronico:" + email);
+
+const users = {
+    userName,
+    email,
+}
+console.log(users)
